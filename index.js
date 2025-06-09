80353a9c79cda786939cc366f6fa86237bbfb436
saidabdulhakim48@gmail.com <saidabdulhakim48@gmail.com>
2025-06-09 23:25:51 +0300

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+function findMatching(drivers, string) {
+  return drivers.filter(driver =>
+    driver.toLowerCase() === string.toLowerCase()
+  );
+}
+
+function fuzzyMatch(drivers, string) {
+  const lowerString = string.toLowerCase();
+  return drivers.filter(driver =>
+    driver.toLowerCase().startsWith(lowerString)
+  );
+}
+
+function matchName(drivers, string) {
+  return drivers.filter(driverObj =>
+    driverObj.name === string
+  );
+}
